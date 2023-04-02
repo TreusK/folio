040473409e35821f39866c2834f38f86143d3878
TreusK <flutri@protonmail.com>
2023-04-02 20:11:34 -0300

    added some projects and images

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const prevButton = document.querySelectorAll(".slide-arrow-prev");
 const nextButton = document.querySelectorAll(".slide-arrow-next");
 
 const projects = [
-  ['./imgs/pr.png', './imgs/pr2.png', './imgs/pr.png'],
-  ['./imgs/pr.png', './imgs/pr2.png', './imgs/pr.png'],
-  ['./imgs/pr.png', './imgs/pr2.png', './imgs/pr.png'],
+  ['./imgs/pokeTeam1.png', './imgs/pokeTeam2.png', './imgs/pokeTeam3.png'],
+  ['./imgs/aniSearch1.png', './imgs/aniSearch2.png', './imgs/aniSearch3.png'],
+  ['./imgs/cli1.png', './imgs/cli2.png', './imgs/cli3.png'],
+  ['./imgs/chat1.png', './imgs/chat2.png', './imgs/chat1.png'],
 ];
 
 prevButton.forEach(elem => elem.addEventListener('click', function(e) {
@@ -14,11 +15,9 @@ prevButton.forEach(elem => elem.addEventListener('click', function(e) {
   if(index == 0) {
     img.src = projects[project][2];
     img.className = 'img2';
-    console.log('img is now ' + img.classList[0])
   } else {
     img.src = projects[project][index-1];
     img.className = 'img' + Number(index-1);
-    console.log('img is now ' + img.classList[0])
   }
 }))
 
@@ -29,10 +28,8 @@ nextButton.forEach(elem => elem.addEventListener('click', function(e) {
   if(index == 2) {
     img.src = projects[project][0];
     img.className = 'img0';
-    console.log('img is now ' + img.classList[0])
   } else {
     img.src = projects[project][index+1];
     img.className = 'img' + Number(index+1);
-    console.log('img is now ' + img.classList[0])
   }
 }))
